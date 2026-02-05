e72f1e9ffe5e40ab9805e1340738305757bfa256
vinaybvciq <vinay.b@commerceiq.ai>
2026-02-05 14:50:04 +0530

    8080 to 80

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -37,12 +37,12 @@ resource "aws_lb_target_group_attachment" "web_attach" {
   count            = length(var.instance)
   target_group_arn = aws_lb_target_group.web_tg.arn
   target_id        = var.instance[count.index]
-  port             = 8080
+  port             = 80
 }
 
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.web_alb.arn
-  port              = 8080
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
